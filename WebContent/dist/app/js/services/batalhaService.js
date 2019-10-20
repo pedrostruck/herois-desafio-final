a1f2bc5e675d5d42771ca23ca662879c988a80dc
Pedro Struck <ppslima@gmail.com>
2019-10-20 11:30:21 -0300

    Adding services that weren't present in last commit.

--- WebContent/dist/app/js/services/batalhaService.js
+++ WebContent/dist/app/js/services/batalhaService.js
@@ -2,11 +2,19 @@ moduleApp.factory("batalhaService", function ($http) {
 
 	const baseUrl = 'http://localhost:8080/curso-hackaton-cdi/batalha';
 
+	const getOponentesUrl = 'http://localhost:8080/curso-hackaton-cdi/batalha/getOponentes';
+
 	const _batalharVsJogador = function (jogador,jogador2) {
 		return $http.post(baseUrl,jogador,jogador2);
 	};
 
+	const _getJogadoresOponentes = function() {
+		return $http.get(getOponentesUrl);
+	};
+
 	return {
-		batalharVsJogador: _batalharVsJogador
-	};	
+		batalharVsJogador: _batalharVsJogador,
+		getJogadoresOponentes: _getJogadoresOponentes
+	};
+
 });
